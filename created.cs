a8af35a9b08ef4c9883243e1df7cb8f91115de19
Burak İDİ <burak.idi@mbis.com.tr>
2018-04-11 13:43:28 +0300

    template

--- created.cs
+++ created.cs
@@ -7,21 +7,6 @@ using RestSharp;
 // IMPORTANT!!!!!!                  //
 // Import RestSharp from NUGET!!!   //
 // -------------------------------- //
-    false
-    false
-    false
-    false
-    false
-    true
-    false
-    false
-    true
-    false
-    false
-    false
-    false
-    false
-
 
 namespace RestClient
 {
@@ -33,61 +18,69 @@ namespace RestClient
         }
 
         public void addPet(){
-            var request = new RestRequest("/pet", Method.POST);
+            var request = new RestRequest(Method.POST);
+            request.Resource = "/pet",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void updatePet(){
-            var request = new RestRequest("/pet", Method.PUT);
+            var request = new RestRequest(Method.PUT);
+            request.Resource = "/pet",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void findPetsByStatus(){
-            var request = new RestRequest("/pet/findByStatus", Method.GET);
+            var request = new RestRequest(Method.GET);
+            request.Resource = "/pet/findByStatus",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void findPetsByTags(){
-            var request = new RestRequest("/pet/findByTags", Method.GET);
+            var request = new RestRequest(Method.GET);
+            request.Resource = "/pet/findByTags",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void getPetById(){
-            var request = new RestRequest("/pet/{petId}", Method.GET);
+            var request = new RestRequest(Method.GET);
+            request.Resource = "/pet/{petId}",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void updatePetWithForm(){
-            var request = new RestRequest("/pet/{petId}", Method.POST);
+            var request = new RestRequest(Method.POST);
+            request.Resource = "/pet/{petId}",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void deletePet(){
-            var request = new RestRequest("/pet/{petId}", Method.DELETE);
+            var request = new RestRequest(Method.DELETE);
+            request.Resource = "/pet/{petId}",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void uploadFile(){
-            var request = new RestRequest("/pet/{petId}/uploadImage", Method.POST);
+            var request = new RestRequest(Method.POST);
+            request.Resource = "/pet/{petId}/uploadImage",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
-    } true -- /store/inventory
+    }
     
     public class storeRestClient{
         private readonly RestSharp.RestClient _restClient;
@@ -97,33 +90,37 @@ namespace RestClient
         }
 
         public void getInventory(){
-            var request = new RestRequest("/store/inventory", Method.GET);
+            var request = new RestRequest(Method.GET);
+            request.Resource = "/store/inventory",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void placeOrder(){
-            var request = new RestRequest("/store/order", Method.POST);
+            var request = new RestRequest(Method.POST);
+            request.Resource = "/store/order",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void getOrderById(){
-            var request = new RestRequest("/store/order/{orderId}", Method.GET);
+            var request = new RestRequest(Method.GET);
+            request.Resource = "/store/order/{orderId}",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void deleteOrder(){
-            var request = new RestRequest("/store/order/{orderId}", Method.DELETE);
+            var request = new RestRequest(Method.DELETE);
+            request.Resource = "/store/order/{orderId}",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
-    } true -- /user
+    }
     
     public class userRestClient{
         private readonly RestSharp.RestClient _restClient;
@@ -133,56 +130,64 @@ namespace RestClient
         }
 
         public void createUser(){
-            var request = new RestRequest("/user", Method.POST);
+            var request = new RestRequest(Method.POST);
+            request.Resource = "/user",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void createUsersWithArrayInput(){
-            var request = new RestRequest("/user/createWithArray", Method.POST);
+            var request = new RestRequest(Method.POST);
+            request.Resource = "/user/createWithArray",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void createUsersWithListInput(){
-            var request = new RestRequest("/user/createWithList", Method.POST);
+            var request = new RestRequest(Method.POST);
+            request.Resource = "/user/createWithList",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void loginUser(){
-            var request = new RestRequest("/user/login", Method.GET);
+            var request = new RestRequest(Method.GET);
+            request.Resource = "/user/login",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void logoutUser(){
-            var request = new RestRequest("/user/logout", Method.GET);
+            var request = new RestRequest(Method.GET);
+            request.Resource = "/user/logout",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void getUserByName(){
-            var request = new RestRequest("/user/{username}", Method.GET);
+            var request = new RestRequest(Method.GET);
+            request.Resource = "/user/{username}",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void updateUser(){
-            var request = new RestRequest("/user/{username}", Method.PUT);
+            var request = new RestRequest(Method.PUT);
+            request.Resource = "/user/{username}",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
         }
 
         public void deleteUser(){
-            var request = new RestRequest("/user/{username}", Method.DELETE);
+            var request = new RestRequest(Method.DELETE);
+            request.Resource = "/user/{username}",
             request.AddHeader("Accept", "application/json");
 
             var response = _restClient.Execute(request);
